79b8d6144d16620fa075d573b5d27e561094b0b3
AnnnnnnK <129075245+AnnnnnnK@users.noreply.github.com>
2023-08-11 18:57:14 +0000

    Deploying to gh-pages from @ AnnnnnnK/goit-js-hw-08@b23faac7aab485a5b64dc7391cf82da2cb18d803 🚀

---
+++ 03-feedback.22e22ab3.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".feedback-form");console.log(e),e.addEventListener("input",(function(e){e.preventDefault();var t=e.target.value;console.log(e.target.value),localStorage.setItem("feedback-form-state",t)}))}();
+//# sourceMappingURL=03-feedback.22e22ab3.js.map
