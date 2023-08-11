b23faac7aab485a5b64dc7391cf82da2cb18d803
AnnnnnnK <129075245+AnnnnnnK@users.noreply.github.com>
2023-08-11 20:56:34 +0200

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
+  
+function timeUpdate(data) {
+        localStorage.setItem('videoplayer-current-time', data.seconds)
+        
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,12 @@
+const form = document.querySelector('.feedback-form');
+console.log(form);
 
+// const formInformation = 
+form.addEventListener('input', handlerForm);
+
+function handlerForm(evt) {
+    evt.preventDefault();
+    const formInformation = evt.target.value;
+    console.log(evt.target.value);
+    localStorage.setItem("feedback-form-state", formInformation);
+}
