b5762f38643d8a7313cbd461f4b56c0b97b988d7
AnnnnnnK <129075245+AnnnnnnK@users.noreply.github.com>
2023-08-12 14:39:55 +0000

    Deploying to gh-pages from @ AnnnnnnK/goit-js-hw-08@438cf840cbbfac9ad4c24fe2863d4ccda58a68ec 🚀

--- 03-feedback.e9907b94.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");console.log(e),e.addEventListener("input",(function(e){e.preventDefault();const t=e.target.value;console.log(e.target.value),localStorage.setItem("feedback-form-state",t)}));
-//# sourceMappingURL=03-feedback.e9907b94.js.map
